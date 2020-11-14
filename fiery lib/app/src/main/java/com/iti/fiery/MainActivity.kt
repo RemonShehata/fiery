e58172ca0af1fc270950f5b160ef5f0c03c2acbd
RemonShehata <RemonGerges94@gmail.com>
2020-11-15 01:42:43 +0200

    Added the lib to the project Tested Admin part

--- fiery lib/app/src/main/java/com/iti/fiery/MainActivity.kt
+++ fiery lib/app/src/main/java/com/iti/fiery/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.iti.fiery
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,5 +28,21 @@ class MainActivity : AppCompatActivity() {
     private fun logAdminInClicked() {
         val email = adminEmail.text.toString()
         val password = adminPassword.text.toString()
+
+        FirebaseAdmin(this, email, password, object : AdminCallbacks {
+            override fun onLoggedin() {
+                Toast.makeText(this@MainActivity, "Admin Logged in", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onLoggedout() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(exception: Exception) {
+                Log.e(TAG, exception.toString())
+            }
+
+        }).singAdminIn()
+
     }
 }
