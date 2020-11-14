3bda041fd59f2c84ce6227be1118c933a5064896
RemonShehata <RemonGerges94@gmail.com>
2020-11-15 01:17:46 +0200

    Prepared click listeners

--- fiery lib/app/src/main/java/com/iti/fiery/MainActivity.kt
+++ fiery lib/app/src/main/java/com/iti/fiery/MainActivity.kt
@@ -2,10 +2,27 @@ package com.iti.fiery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setclickListeners()
+    }
+
+    private fun setclickListeners() {
+        logAdminIn.setOnClickListener { logAdminInClicked() }
+        createNewUser.setOnClickListener { createNewUserClicked() }
+    }
+
+    private fun createNewUserClicked() {
+        val email = userEmail.text.toString()
+        val password = userPassword.text.toString()
+    }
+
+    private fun logAdminInClicked() {
+        val email = adminEmail.text.toString()
+        val password = adminPassword.text.toString()
     }
 }
